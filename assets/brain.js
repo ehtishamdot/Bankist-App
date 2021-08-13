565c8467a291fe460fede26ecaaf12afdcb13a8a
Ehtisham Ul Haq <ahtionpc1@gmail.com>
2021-08-14 00:40:20 +0500

    first commit

---
+++ assets/brain.js
@@ -0,0 +1,28 @@
+
+const account1 = {
+  owner: 'Ehtisham Ul Haq',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Muhammad Faisal',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Shehnaz Hussain',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Faqir Hussain',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
